f96e0716df47cba8da0e29bcbcf50abfeef088aa
floracai <floracai254@gmail.com>
2021-10-16 21:50:55 -0400

    actual code

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Flora Cai on 10/2/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
